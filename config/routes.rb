d3d94490ca5e309c55962c00d68c55f79e4f1be1
Ivan Kornilov <vano468@gmail.com>
2016-02-29 14:38:07 +0300

    Change /version route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   end
 
   get 'status',  to: Proc.new { [200, {}, ['']] }
-  get 'version', to: Proc.new { [200, {}, [Settings.version]] }
+  get 'version', to: Proc.new { [200, {}, ["#{Settings.app_name} #{Settings.version} (#{Rails.env})"]] }
   match ':not_found' => 'errors#not_found', constraints: { not_found: /.*/ }, via: [:all]
 end
