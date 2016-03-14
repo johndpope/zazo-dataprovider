da620d5d438a1a5b54293b877a23c8a406755491
Ivan Kornilov <vano468@gmail.com>
2016-03-14 19:18:05 +0300

    Add `ActiveFriends` users query

--- app/queries/users/queries/attributes/friends.rb
+++ app/queries/users/queries/attributes/friends.rb
@@ -1,11 +1,17 @@
 class Users::Queries::Attributes::Friends < Users::Queries::Attributes::Base
   def value
-    connections.each_with_object(Set.new) do |conn, memo|
-      memo.merge([conn.target.mkey, conn.creator.mkey])
-    end.delete(user.mkey).to_a
+    select_connections do |conn|
+      [conn.target.mkey, conn.creator.mkey]
+    end
   end
 
-  private
+  protected
+
+  def select_connections
+    connections.each_with_object(Set.new) do |conn, memo|
+      memo.merge(yield(conn))
+    end.delete(user.mkey).to_a
+  end
 
   def connections
     user.connections.includes(:target, :creator)
